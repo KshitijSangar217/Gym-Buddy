bd778374da94f7c003690a3b2efcd0f1633d45b5
KshitijSangar217 <kshitijsangar@gmail.com>
2021-04-29 16:09:21 +0530

    Added logout button for member dashboard

--- app/src/main/java/com/example/pbl_android/MemberMainPage.java
+++ app/src/main/java/com/example/pbl_android/MemberMainPage.java
@@ -2,7 +2,11 @@ package com.example.pbl_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MemberMainPage extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MemberMainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_main_page);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),SelectType.class));
     }
 }

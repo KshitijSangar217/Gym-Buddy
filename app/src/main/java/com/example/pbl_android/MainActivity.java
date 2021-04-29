20f695c0383569950cb958762a163503807226be
prathmeshpatil12 <prathmeshpatil0126@gmail.com>
2021-04-29 16:34:06 +0530

    Fixed main page issue

--- app/src/main/java/com/example/pbl_android/MainActivity.java
+++ app/src/main/java/com/example/pbl_android/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.pbl_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +31,39 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent;
                 if(fAuth.getCurrentUser() != null){
-                    intent=new Intent(MainActivity.this, TrainerMainPage.class);
-                }
-                else{
-                    intent=new Intent(MainActivity.this, SelectType.class);
-                }
+                    //intent=new Intent(MainActivity.this, TrainerMainPage.class);
+                    String id = fAuth.getCurrentUser().getUid();
+                    Log.d("Prathmesh", "run: id : "+id);
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Trainers");
+                    ref.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            Log.d("Prathmesh", "onDataChange: "+snapshot.exists());
+                            if (snapshot.exists())
+                            {
+                                Intent i = new Intent(MainActivity.this, TrainerMainPage.class);
+                                startActivity(i);
+                                finish();
+                            }
+                            else
+                            {
+                                Intent i = new Intent(MainActivity.this, MemberMainPage.class);
+                                startActivity(i);
+                                finish();
+                            }
+                        }
 
-                startActivity(intent);
-                finish();
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {}
+                    });
+                }
+                else
+                {
+                    Intent i = new Intent(MainActivity.this, SelectType.class);
+                    startActivity(i);
+                    finish();
+                }
             }
         },3000);
     }
